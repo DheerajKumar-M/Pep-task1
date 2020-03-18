6b778dcdd40f1025246488577949c35165343f17
Dheeraj Kumar <17211a05f6@bvrit.ac.in>
2020-03-18 18:46:50 +0530

    first commit

---
+++ Example.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+class Example
+{
+public static void main(String args[])
+{
+int c,a,b;
+Scanner sc=new Scanner(System.in);
+a=sc.nextInt();
+b=sc.nextInt();
+c=a+b;
+System.out.println(c);
+}
+}
